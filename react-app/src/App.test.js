ae52d7887a18aea64871ebcab6a9044ed1d2b7f1
Aditya_Upadhyay <chahatupadhyay2006@gmail.com>
2026-09-01 11:36:13 +0530

    Update test to check for modified text

--- react-app/src/App.test.js
+++ react-app/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/learnxyzeerrrr react/i);
   expect(linkElement).toBeInTheDocument();
 });
